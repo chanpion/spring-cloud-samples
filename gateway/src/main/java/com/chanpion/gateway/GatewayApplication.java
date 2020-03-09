a75184ac37e91aca0b6b26f4fcaa9fd616797ef4
April.Chen <forchpeng@163.com>
2020-03-09 20:06:51 +0800

    添加Gateway网关模块

---
+++ gateway/src/main/java/com/chanpion/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.chanpion.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author April Chen
+ * @date 2020/3/9 15:10
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
