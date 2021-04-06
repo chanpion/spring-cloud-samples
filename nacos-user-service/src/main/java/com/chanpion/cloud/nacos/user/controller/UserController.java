d0cee8feafdcf41d6f72d5487a644445fb58ecf8
April.Chen <forchpeng@163.com>
2021-04-06 16:54:26 +0800

    add sentinel

--- nacos-user-service/src/main/java/com/chanpion/cloud/nacos/user/controller/UserController.java
+++ nacos-user-service/src/main/java/com/chanpion/cloud/nacos/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.chanpion.cloud.nacos.user.controller;
 
+import com.alibaba.csp.sentinel.adapter.spring.webflux.exception.SentinelBlockExceptionHandler;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.chanpion.cloud.common.CommonResponse;
 import com.chanpion.cloud.common.model.User;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @RequestMapping("/{id}")
+    @SentinelResource(value = "",blockHandler = "doOnBlock",blockHandlerClass = SentinelBlockExceptionHandler.class)
     public CommonResponse getUser(@PathVariable Long id) {
         return new CommonResponse(new User(10L, "nacos"));
     }
